729e894169ccfb9e0bb0933713fad98e9ae1cb3f
Parsa Ahmadi Nasab Emran <parsatempleowl@brandeis.edu>
2026-01-30 13:16:22 -0500

    Add product model scaffold and commented out verbatimModuleSyntax: true, is tsconfig.json file

---
+++ src/models/Product.ts
@@ -0,0 +1,33 @@
+export class Product {
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    discountPercentage: number;
+    category: string;
+
+    constructor(
+        id: number,
+        title: string,
+        description: string,
+        price: number,
+        discountPercentage: number,
+        category: string,
+    ) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.discountPercentage = discountPercentage;
+        this.category = category;
+    }
+
+    displayDetails(): void {
+        // TODO: Implement display logic
+    }
+
+    getPriceWithDiscount(): number {
+        // TODO: Implement discount calculation
+        return this.price;
+    }
+}
